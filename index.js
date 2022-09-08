e753e1d274bbd4c01ceac6b978d9653aac37a281
Saoki11111 <t.aoki11111@gmail.com>
2022-09-08 23:57:52 +0900

    Use Webpack

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const app = express();
 // 3000番ポートでWebサーバを立てる
 app.listen(3000);
 
+app.use(express.static('assets'));
+
 // https://localhost:3000 にアクセスがあったら ssr() を返す
 app.get('/', (_, res) => {
   res.send(ssr());
